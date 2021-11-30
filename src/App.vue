de9f5d8719fb2700ec77a4e6d60a5ecacb055f84
Gergő <s.gergo.az@gmail.com>
2021-11-30 14:07:49 +0100

    todo komponens kialakítása

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <todo/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import todo from './components/Todo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    todo
   }
 }
 </script>
